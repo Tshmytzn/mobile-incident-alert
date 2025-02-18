b2242469d541008e49d221eb952b80b07a5c8b42
Tish <tbtizon.chmsu@gmail.com>
2025-02-18 23:31:57 +0800

    Admin Login

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//User Login
 Route::get('/login', function () {
     return view('login');
+});
+
+//Admin Login
+Route::get('/administrator', function () {
+    return view('Adminlogin');
 });
