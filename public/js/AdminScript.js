3e9e88295ce9f77286effc415a5095dec92e0d99
JP <jpubas@gmail.com>
2025-02-24 23:03:22 +0800

    add sweet alert

--- public/js/AdminScript.js
+++ public/js/AdminScript.js
@@ -8,15 +8,40 @@ function AdminLogin(id) {
 
     let formData = new FormData(form); // Collect all form inputs dynamically
     
+    
     sendRequest("POST", "/save-data", formData, function (error, response) {
         if (error) {
             console.error("POST Error:", error, response);
         } else {
-            window.location.replace("/dashboard");
+            Swal.fire({
+                toast: true,
+                position: "top-end",
+                icon: "success",
+                title: "Signed in successfully",
+                showConfirmButton: false,
+                timer: 1500,
+            }).then(() => {
+                window.location.replace("/dashboard");
+            });
         }
     });
 }
 
+// Swal.fire({
+//     title: "Are you sure?",
+//     text: "You won't be able to revert this!",
+//     icon: "warning",
+//     showCancelButton: true,
+//     confirmButtonText: "Yes, delete it!",
+//     cancelButtonText: "No, cancel!",
+//     reverseButtons: true,
+// }).then((result) => {
+//     if (result.isConfirmed) {
+//         Swal.fire("Deleted!", "Your file has been deleted.", "success");
+//     } else if (result.dismiss === Swal.DismissReason.cancel) {
+//         Swal.fire("Cancelled", "Your file is safe.", "error");
+//     }
+// });
 
 // sendRequest("GET", getUrl, null, function (error, response) {
 //     if (error) {
--- resources/views/Administrator/components/scripts.blade.php
+++ resources/views/Administrator/components/scripts.blade.php
@@ -11,4 +11,7 @@
     {{-- Datatables --}}
      <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
      <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+
 
